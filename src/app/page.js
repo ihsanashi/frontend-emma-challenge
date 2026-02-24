42b65eea012cac1c92243dddb57a71046815ff4a
Ahmad Ihsan <ihsanashi@gmail.com>
2026-02-24 11:54:43 +0800

    chore: bump tailwind to v4

--- src/app/page.js
+++ src/app/page.js
@@ -50,14 +50,14 @@ export default function Home() {
           <input
             type="text"
             placeholder="Search for books..."
-            className="text-black flex-grow border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="text-black grow border rounded-md px-4 py-2 focus:outline-hidden focus:ring-2 focus:ring-blue-500"
             value={userInput}
             onChange={(event) => {
               setUserInput(event.target.value);
             }}
           />
           <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none"
+            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-hidden"
             onClick={() => fetchBooks(userInput)}
           >
             Submit
